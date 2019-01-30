507160aaec844079ef610f9aa9a9bdb8c571d02a
EduDinizO <edu_diniz@hotmail.com>
2019-01-30 16:47:42 -0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
+import Layout from './components/layout/Layout';
 
 class App extends Component {
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col md={6}>
-                        <a href={'https://react-bootstrap.netlify.com/layout/grid/'} target={'blank'}>
-                            Documentação do bootstrap layout
-                        </a>
-                    </Col>
-                    <Col md={6}>
-                        <a href={'https://react-bootstrap.netlify.com/components/alerts/'} target={'blank'}>
-                            Documentação dos componentes do bootstrap
-                        </a>
-                    </Col>
-                </Row>
-            </Container>
+            <Layout/>
         );
     }
 }
