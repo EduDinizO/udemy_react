d48236f1b1909398ddd507905e2b065c631dc049
EduDinizO <edu_diniz@hotmail.com>
2019-01-30 15:42:53 -0200

    added react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <a href={'https://react-bootstrap.netlify.com/layout/grid/'} target={'blank'}>
+                            Documentação do bootstrap layout
+                        </a>
+                    </Col>
+                    <Col md={6}>
+                        <a href={'https://react-bootstrap.netlify.com/components/alerts/'} target={'blank'}>
+                            Documentação dos componentes do bootstrap
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
